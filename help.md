6b0e6e4147c4f7bb25da6ebd7469f05dd551c020
kdhrubo78 <dhrubojyoti.kayal@camunda.com>
2024-10-07 13:54:39 -0500

    Initial commit

---
+++ help.md
@@ -0,0 +1,16 @@
+## Install JBANG
+
+```shell
+
+curl -Ls https://sh.jbang.dev | bash -s - app setup
+
+```
+
+
+## Run Script
+
+```shell
+
+./optimize.java
+
+```
---
+++ optimize.java
@@ -0,0 +1,28 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//DEPS info.picocli:picocli:4.6.3
+
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "optimize", mixinStandardHelpOptions = true, version = "optimize 0.1",
+        description = "optimize made with jbang")
+class optimize implements Callable<Integer> {
+
+    @Parameters(index = "0", description = "The greeting to print", defaultValue = "World!")
+    private String greeting;
+
+    public static void main(String... args) {
+        int exitCode = new CommandLine(new optimize()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+        System.out.println("Hello " + greeting);
+        return 0;
+    }
+}
